5c1004f75751ae937c52c4cd4a2ee4a2e5f86a83
pjnalls <pjnalls.js@gmail.com>
2021-12-06 06:21:59 -0600

    Declare `handleMenuToggle` & `main` functions.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("`app.js` ran.");
+const handleMenuToggle = () =>
+  document.getElementById("menu").classList.toggle("open");
+
+const main = () => {
+  document
+    .getElementById("menu")
+    .addEventListener("click", handleMenuToggle);
+};
+
+main();
