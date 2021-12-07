55b2afbb644948c681fde1878c1db9ba985ce077
pjnalls <pjnalls.js@gmail.com>
2021-12-07 08:05:10 -0600

    Add logic for `"overlay"` ID for animations.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const handleMenuToggle = () => {
-  ["header", "menu", "overlay"].forEach((id) =>
-    document.getElementById(id).classList.toggle("open")
-  );
+  ["header", "menu", "overlay"].forEach((id) => {
+    document.getElementById(id).classList.toggle("open");
+    if (id === "overlay") {
+      const overlay = document.getElementById(id).classList;
+      overlay.contains("open")
+        ? (overlay.remove("fade-out"), overlay.add("fade-in"))
+        : (overlay.remove("fade-in"), overlay.add("fade-out"));
+    }
+  });
 };
 
 const main = () => {
